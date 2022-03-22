d8b9153b0e2fb8651cb70b4e8971c3ec07892459
Neeta_Nayak <neeta.nayak@mitel.com>
2022-03-22 12:35:18 +0530

    Second commit

--- src/main/java/net/java/springboot/controller/EmployeeController.java
+++ src/main/java/net/java/springboot/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import net.java.springboot.model.Employee;
 import net.java.springboot.service.EmployeeService;
